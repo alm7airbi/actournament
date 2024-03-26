23da571c4ca4a380346614a4f4374c3ae534d078
mr.salt <mr.alm7airbi@gmail.com>
2024-03-26 16:36:41 +0400

    update

---
+++ src/main/java/actournament/actournaments/ACtournaments.java
@@ -0,0 +1,17 @@
+package actournament.actournaments;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ACtournaments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
